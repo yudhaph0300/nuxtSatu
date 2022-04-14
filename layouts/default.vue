1499f49704e23c17217b0dbe71bb3036dce804f1
Mohammad Yudha Pamungkas <93898512+yudhaph0300@users.noreply.github.com>
2022-04-14 20:58:36 +0700

    Latihan

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,102 +1,79 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
+    <v-app-bar tile dark elevation="2" >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <h3 class="ml-4">News App</h3>
+      <v-spacer></v-spacer>
+      
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field class="mt-6 ml-2" filled rounded dense append-icon="mdi-magnify"/>
+      </v-col>
+    </v-app-bar>
+
+     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
+      absolute
+      bottom
+      temporary
     >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+      <v-list
+        nav
+        dense
+      >
+      <v-list-item class="my-2">
+          <v-list-item-title>
+            <h1>Category</h1>
+          </v-list-item-title>
+      </v-list-item>
+
+      <v-divider></v-divider>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
+          <v-list-item> 
+            <v-list-item-title @click="setCategory('games')">Games</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title @click="setCategory('bitcoin')">Bitcoin</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title @click="setCategory('putin')">Vladimir Putin</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
   name: 'DefaultLayout',
-  data() {
-    return {
-      clipped: false,
+  data: () => ({
       drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'HomeView',
-          to: '/homeview',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'DetailView',
-          to: '/detail/:id',
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
+      group: null,
+  }),
+  watch: {
+      group () {
+        this.drawer = false
+      },
   },
+  mounted() {
+    this.$store.dispatch("fetchNews")
+  },
+  methods: {
+    setCategory(keyword) {
+      this.$store.dispatch("fetchNews", keyword)
+    }
+  }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,60 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div class="home">
+    <v-card
+    v-for="(news, index) in newsGroup" :key="index"
+    class="mx-auto my-10"
+    max-width="80%"
+    >
+    
+      <v-img
+        height="500"
+        :src="news.urlToImage"
+      ></v-img>
+
+      <v-card-title>{{ news.title }}</v-card-title>
+
+      <v-card-text>
+        <div
+        v-if="newsGroup[index].author === null"
+        class="my-1 text-subtitle-2">
+          Author  : Anonymous
+        </div>
+
+        <div
+        v-else
+        class="my-1 text-subtitle-2">
+          Author  : {{ news.author }}
+        </div>
+
+        <div>{{ news.publishedAt }}</div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+      <v-card-actions>
+        <v-btn
+          class="px-5"
+          color="deep-purple"
+          dark large
+          :to="'/detail/id' + index"
+        >
+          read this news          
+          
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'HomeView',
+  
+  computed: {
+    newsGroup() {
+      return this.$store.state.newsList
+    }
+  }
 }
 </script>
